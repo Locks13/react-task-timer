af8ac4725a89413f854f073f92e08d522bed55d5
Lucas Toffoli <lktoffoli@gmail.com>
2026-07-09 13:58:19 -0300

    reducer and singleton

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,25 @@
+import { taskModel } from "../../models/TaskModel";
+
+export enum TaskActionTypes {
+  START_TASK = "START_TASK",
+  INTERRUPT_TASK = "INTERRUPT_TASK",
+  RESET_TASK = "RESET_TASK",
+}
+
+export type TaskActionModelWithPayload =
+  | {
+      type: TaskActionTypes.START_TASK;
+      payload: taskModel;
+    }
+  | {
+      type: TaskActionTypes.INTERRUPT_TASK;
+      payload: taskModel;
+    };
+
+export type TaskActionModelWithoutPayload = {
+  type: TaskActionTypes.RESET_TASK;
+};
+
+export type TaskActionModel =
+  | TaskActionModelWithPayload
+  | TaskActionModelWithoutPayload;
